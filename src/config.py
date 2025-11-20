a204646e3be51f0eb7ef0b2ac9a4f09c94cbda11
Tim <47699597+hidiodi@users.noreply.github.com>
2025-11-20 14:30:37 +0100

    back to finetuning wofost

--- src/config.py
+++ src/config.py
@@ -22,7 +22,7 @@ SCRIPTS_TO_RUN = [
     #"src/02_models/Wofost7.1/build_initial_conditions.py",
     "src/02_models/Wofost7.1/build_site_data.py",
     "src/02_models/Wofost7.1/build_genetic_parameters.py",
-    #"src/02_models/Wofost7.1/build_forecast_weather.py",
+    "src/02_models/Wofost7.1/build_forecast_weather.py",
     "src/02_models/Wofost7.1/run_wofost_pipeline.py",
     "src/02_models/Wofost7.1/validation_dashboard.py",
     #"src/02_models/Wofost7.1/create_trendModel.py",
@@ -84,7 +84,7 @@ WEATHER_END_YEAR = 2024
 # --- WOFOST Model Configuration ---
 WOFOST_CONFIG = {
     # max range 1982 - 2024 and None to run all districts
-    'START_YEAR': 1982, 'END_YEAR': 2024, 'DISTRICT_LIMIT': 20,
+    'START_YEAR': 1982, 'END_YEAR': 2024, 'DISTRICT_LIMIT': 10,
     'FILE_PATHS': {
         'HISTORICAL_DAILY_WEATHER_DIR': DATA_DIR / '02_intermediate/daily_weather',
         'CORRECT_WEATHER_DIR': DATA_DIR / '04_feature/weather_district_daily',
@@ -112,11 +112,11 @@ WOFOST_CONFIG = {
     'GENERIC_SITE': {'LATITUDE': 52.0, 'LONGITUDE': 10.0, 'ELEVATION': 50.0},
     'ANALOG_YEAR_CONFIG': {'NUM_ANALOGS': 5, 'MIN_YEARS_FOR_FIT': 10, },
     'CONSTANTS': {
-        'DMC_SUGARBEET': 0.25, # Keep at 25%
+        'DMC_SUGARBEET': 0.25,
         'INITIAL_ROOTING_DEPTH_CM': 10.0,
         'SOIL_PARTICLE_DENSITY': 2.65,
-        'HARVEST_LOSS_FACTOR': 0.96, # Ensure this is 0.85
-        'MAX_ROOTING_DEPTH_GLOBAL_CAP_CM': 120.0 # INCREASE this from 70.0 to 120.0
+        'HARVEST_LOSS_FACTOR': 0.95,
+        'MAX_ROOTING_DEPTH_GLOBAL_CAP_CM': 150.0
     },
 
 
